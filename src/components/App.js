7599e7f3af934dbf8eaaa7dbb9004740a77e4946
sangitasarkar74 <sangitasarkar@gmail.com>
2023-02-12 23:47:22 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  //code here 
-  const [style, setStyle] = useState(true);
+  const [changeColor, setChangeColor] = useState(false);
 
-  const changeStyle = () => {
-    return setStyle(!style);
-  }
+  const handleClick = () => {
+    setChangeColor(!changeColor);
+  };
 
   return (
     <div id="main">
-      <p className={style ? "redColor" : "blueColor"} >Newton School</p>
-      <button id='button' onClick={changeStyle}>Change Style</button>
+      <p className={`${changeColor === true ? "blueColor" : "redColor"}`}>
+        Newton School
+      </p>
+      <button id="button" onClick={handleClick}>
+        Change Style
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
